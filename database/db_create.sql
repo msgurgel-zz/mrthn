475b1db90f8d25348f56d23a60110aa843b3920a
Mateus Gurgel <mgurgel5855@conestogac.on.ca>
2020-03-13 16:17:57 -0400

    :card_file_box: Update db_create.sql
    With this, the script can be piped into heroku pg:psql

--- database/db_create.sql
+++ database/db_create.sql
@@ -1,31 +1,27 @@
-DROP database marathon;
-CREATE DATABASE marathon;
+DROP SCHEMA public CASCADE;
+CREATE SCHEMA public;
 SET SEARCH_PATH = "public";
 
 CREATE TABLE "user"(
     id SERIAL PRIMARY KEY
 );
-​
 CREATE TABLE client(
     id     SERIAL       PRIMARY KEY,
     name   VARCHAR(50)  NOT NULL,
     secret BYTEA,
     password TEXT NOT NULL
 );
-​
 CREATE TABLE userbase(
     id        SERIAL  PRIMARY KEY,
     user_id   INTEGER REFERENCES "user"(id),
     client_id INTEGER REFERENCES client(id)
 );
-
 CREATE TABLE platform(
     id          SERIAL      PRIMARY KEY,
     name        VARCHAR(64) NOT NULL,
     domain      VARCHAR(128) NOT NULL
 );
 CREATE INDEX plat_name_index ON platform(name);
-
 CREATE TABLE credentials(
     id                SERIAL      PRIMARY KEY,
     user_id           INTEGER     REFERENCES "user"(id),
